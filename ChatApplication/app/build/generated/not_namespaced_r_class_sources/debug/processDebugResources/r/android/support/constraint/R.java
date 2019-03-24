6abe3fd16ec3ff37b608eed0cf90fdbdbd4bbfb5
Christine-Mossiah <christine.mossiah3@gmail.com>
2019-03-23 21:25:48 -0400

    fixed conflicts from previous commit

--- ChatApplication/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ ChatApplication/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -73,8 +73,6 @@ public final class R {
     public static final class id {
         private id() {}
 
-<<<<<<< HEAD
-<<<<<<< HEAD
         public static final int bottom = 0x7f070023;
         public static final int end = 0x7f070039;
         public static final int gone = 0x7f070040;
@@ -89,27 +87,6 @@ public final class R {
         public static final int start = 0x7f070089;
         public static final int top = 0x7f070099;
         public static final int wrap = 0x7f0700a2;
-=======
-=======
->>>>>>> master
-        public static final int bottom = 0x7f070022;
-        public static final int end = 0x7f070037;
-        public static final int gone = 0x7f07003e;
-        public static final int invisible = 0x7f070049;
-        public static final int left = 0x7f07004b;
-        public static final int packed = 0x7f07005b;
-        public static final int parent = 0x7f07005c;
-        public static final int percent = 0x7f070060;
-        public static final int right = 0x7f070064;
-        public static final int spread = 0x7f07007e;
-        public static final int spread_inside = 0x7f07007f;
-        public static final int start = 0x7f070084;
-        public static final int top = 0x7f070094;
-        public static final int wrap = 0x7f07009d;
-<<<<<<< HEAD
->>>>>>> origin/master
-=======
->>>>>>> master
     }
     public static final class styleable {
         private styleable() {}
