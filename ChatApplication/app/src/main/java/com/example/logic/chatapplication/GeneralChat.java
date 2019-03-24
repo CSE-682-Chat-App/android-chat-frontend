f98667e8385747fe166a57322bfba7bf00c4abc5
Christine-Mossiah <christine.mossiah3@gmail.com>
2019-03-23 20:59:04 -0400

    added chat activity

---
+++ ChatApplication/app/src/main/java/com/example/logic/chatapplication/GeneralChat.java
@@ -0,0 +1,50 @@
+package com.example.logic.chatapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class GeneralChat extends AppCompatActivity {
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_general_chat);
+
+        editText = (EditText) findViewById(R.id.editText);
+    }
+
+    public void sendMessage(View view) {
+        String message = editText.getText().toString();
+        if (message.length() > 0) {
+
+        }
+    }
+/*
+    @Override
+    public void onMessage(Room room, final JsonNode json, final Member member) {
+        // To transform the raw JsonNode into a POJO we can use an ObjectMapper
+        final ObjectMapper mapper = new ObjectMapper();
+        try {
+            // member.clientData is a MemberData object, let's parse it as such
+            final MemberData data = mapper.treeToValue(receivedMessage.getMember().getClientData(), MemberData.class);
+            // if the clientID of the message sender is the same as our's it was sent by us
+            boolean belongsToCurrentUser = receivedMessage.getClientID().equals(scaledrone.getClientID());
+            // since the message body is a simple string in our case we can use json.asText() to parse it as such
+            // if it was instead an object we could use a similar pattern to data parsing
+            final Message message = new Message(receivedMessage.getData().asText(), data, belongsToCurrentUser);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    messageAdapter.add(message);
+                    // scroll the ListView to the last added element
+                    messagesView.setSelection(messagesView.getCount() - 1);
+                }
+            });
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    } */
+}
